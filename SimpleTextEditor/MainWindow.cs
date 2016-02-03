a18fb12a29035f4a5259736d5782fc3bbcefac8a
unknown-user <unknown-user@192.168.0.198>
2016-02-03 21:09:35 +0100

    Initial check-in of module SimpleTextEditor

---
+++ SimpleTextEditor/MainWindow.cs
@@ -0,0 +1,42 @@
+using System;
+using Gtk;
+using System.IO;
+
+public partial class MainWindow : Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected virtual void OnButtonClearClicked(object sender, System.EventArgs e)
+	{
+		//code executed when the Clear button is clicked
+		textview1.Buffer.Text = ""; //clears the buffer displayed by the TextView
+	}
+
+	protected virtual void OnButtonSaveClicked(object sender, System.EventArgs e)
+	{
+		//code executed when the Copy button is clicked
+		StreamWriter sw = new StreamWriter("Test.txt");
+		sw.Write(textview1.Buffer.Text); //Write textview1 text to file
+		textview1.Buffer.Text = "Saved to file !"; //Notify user
+		sw.Close();
+	}
+
+	protected virtual void OnButtonUcaseClicked (object sender, System.EventArgs e)
+	{
+		textview1.Buffer.Text = textview1.Buffer.Text.ToUpper();
+	}
+
+	protected virtual void OnButtonLcaseClicked (object sender, System.EventArgs e)
+	{
+		textview1.Buffer.Text = textview1.Buffer.Text.ToLower();
+	}
+}
